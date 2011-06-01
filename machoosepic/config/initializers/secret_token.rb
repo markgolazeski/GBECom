41a54de7316cbf14c3df1d5bf3a82d4f83d43c67
Mark Golazeski <mark.golazeski@gmail.com>
2011-06-01 00:10:10 -0700

    adding machoosepic rails files

---
+++ machoosepic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Machoosepic::Application.config.secret_token = 'e925cb813a2ce9014828014589bd607ffb446911109eb83bb33536cf959afbeb6cafa8b442497824f13633fbb64b484b7be6629c5669dc72017d678dd9dc1efc'
