1d77a77c9aa9e183118e6edcfd916bb3674ec284
Mark Golazeski <mark.golazeski@gmail.com>
2011-06-09 23:30:55 -0700

    Adding schema file

--- machoosepic/db/schema.rb
+++ machoosepic/db/schema.rb
@@ -10,11 +10,24 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110601071719) do
+ActiveRecord::Schema.define(:version => 20110604050453) do
 
   create_table "photos", :force => true do |t|
-    t.string   "href"
+    t.string   "href",                                       :null => false
     t.string   "owner"
+    t.integer  "elo_rating",               :default => 1000
+    t.integer  "match_count",              :default => 0
+    t.string   "is_portrait", :limit => 1, :default => "N"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "votes", :force => true do |t|
+    t.string   "address"
+    t.integer  "photo_id_winner"
+    t.integer  "photo_id_loser"
+    t.integer  "winner_original_rating"
+    t.integer  "loser_original_rating"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
